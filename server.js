92b53f330a1c6cd16e4b0afd717dc95888cf9232
Devon Reid <fireborn1003@gmail.com>
2020-05-07 18:09:40 +0600

    Added colors as dependency for terminal highlighting

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
+
 const connectDB = require('./config/db');
 
 // load env vars
@@ -24,13 +26,13 @@ app.use('/api/v1/bootcamps', bootcamps);
 const { PORT = 5000 } = process.env;
 
 const server = app.listen(PORT, () => {
-	console.log(`Server listening in ${process.env.NODE_ENV} mode on port ${PORT}`);
+	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold);
 });
 
 // handle unhandled promise rejections
 
 process.on('unhandledRejection', (err, promise) => {
-	console.log(`Error: ${err.message}`);
+	console.log(`Error: ${err.message}`.red);
 	// Close server and exit process
 	server.close(() => process.exit(1));
 });
