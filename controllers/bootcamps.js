c234f98ff55193d229b27f562708700dbe1e509f
Devon Reid <fireborn1003@gmail.com>
2020-05-09 11:04:04 +0600

    Ch 5 Vid 3
    * Passed new ErrorResponse or err to errorHandler
    * errorHandler responses with the correct response based on error type

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -9,7 +9,7 @@ exports.getBootcamps = async (req, res, next) => {
 		const bootcamps = await Bootcamp.find({});
 		res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps });
 	} catch (err) {
-		res.status(400).json({ success: false });
+		next(err);
 	}
 };
 
@@ -19,10 +19,10 @@ exports.getBootcamps = async (req, res, next) => {
 exports.getBootcamp = async (req, res, next) => {
 	try {
 		const bootcamp = await Bootcamp.findById(req.params.id);
-		if (!bootcamp) next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+		if (!bootcamp) return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
 		res.status(200).json({ success: true, data: bootcamp });
 	} catch (err) {
-		next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+		next(err);
 	}
 };
 
@@ -34,7 +34,7 @@ exports.createBootcamp = async (req, res, next) => {
 		const bootcamp = await Bootcamp.create(req.body);
 		res.status(201).json({ success: true, data: bootcamp });
 	} catch (err) {
-		res.status(400).json({ success: false });
+		next(err);
 	}
 };
 
@@ -44,10 +44,10 @@ exports.createBootcamp = async (req, res, next) => {
 exports.updateBootcamp = async (req, res, next) => {
 	try {
 		const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-		if (!bootcamp) return res.status(200).json({ success: false });
+		if (!bootcamp) return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
 		res.status(200).json({ success: true, data: bootcamp });
 	} catch (err) {
-		res.status(400).json({ success: false });
+		next(err);
 	}
 };
 
@@ -57,9 +57,9 @@ exports.updateBootcamp = async (req, res, next) => {
 exports.deleteBootcamp = async (req, res, next) => {
 	try {
 		const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
-		if (!bootcamp) return res.status(200).json({ success: false });
+		if (!bootcamp) return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
 		res.status(200).json({ success: true, data: bootcamp });
 	} catch (err) {
-		res.status(400).json({ success: false });
+		next(err);
 	}
 };
--- middleware/error.js
+++ middleware/error.js
@@ -1,7 +1,17 @@
+const ErrorResponse = require('../utils/errorResponse');
+
 function errorHandler(err, req, res, next) {
 	console.log(err.stack.red);
 
-	res.status(err.statusCode || 500).json({ success: false, error: err.message || 'Server Error' });
+	let error = { ...err };
+	error.message = err.message;
+
+	// Mongoose bad objectid
+	if (err.name === 'CastError') {
+		const message = `Resource not found with id of ${err.value}`;
+		error = new ErrorResponse(message, 404);
+	}
+	res.status(error.statusCode || 500).json({ success: false, error: error.message || 'Server Error' });
 }
 
 module.exports = errorHandler;
