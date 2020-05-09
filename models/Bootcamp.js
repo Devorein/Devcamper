f935ec296f6cee33e07151d4f73f37294da734b8
Devon Reid <fireborn1003@gmail.com>
2020-05-09 11:56:25 +0600

    Ch 5 Vid 7
    * Added node-geocoder package
    * Converted location field to geojson before bootcamp save

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 
+const geocoder = require('../utils/geoCoder');
+
 const BootcampSchema = new mongoose.Schema(
 	{
 		name: {
@@ -39,12 +41,10 @@ const BootcampSchema = new mongoose.Schema(
 			// GeoJSON Point
 			type: {
 				type: String,
-				// required: true,
 				enum: ['Point']
 			},
 			coordinates: {
 				type: [Number],
-				// required: true,
 				index: '2dsphere'
 			},
 			formattedAddress: String,
@@ -106,6 +106,26 @@ const BootcampSchema = new mongoose.Schema(
 
 BootcampSchema.pre('save', function (next) {
 	this.slug = slugify(this.name, { lower: true });
+	next();
+});
+
+// Geocode and create location field
+
+BootcampSchema.pre('save', async function (next) {
+	const loc = await geocoder.geocode(this.address);
+	this.location = {
+		type: 'Point',
+		coordinates: [loc[0].longitude, loc[0].latitude],
+		formattedAddress: loc[0].formattedAddress,
+		street: loc[0].streetName,
+		city: loc[0].city,
+		state: loc[0].stateCode,
+		zipcode: loc[0].zipcode,
+		country: loc[0].countryCode
+	};
+
+	// Do not save address
+	this.address = undefined;
 	next();
 });
 
