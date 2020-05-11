ecb95f04c5133fecc98b17bbced626d10744d802
Devon Reid <fireborn1003@gmail.com>
2020-05-11 16:37:52 +0600

    Ch 6 Vid 9
    * Get a specific course
    * Create a course for a specific bootcamp

--- routes/courses.js
+++ routes/courses.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
 
-const { getCourses } = require('../controllers/courses');
+const { getCourses, getCourse, createCourse } = require('../controllers/courses');
 
-router.route('/').get(getCourses);
+router.route('/').get(getCourses).post(createCourse);
+router.route('/:id').get(getCourse);
 
 module.exports = router;
